cf761db00b7d6e51c008ddef418dce4df85de033
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-03-07 17:40:26 +0200

    Add PostDelete

--- forum/urls.py
+++ forum/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('<int:year>/<int:month>/<slug:slug>/', views.PostShow.as_view(), name='post'),
     path('post/add/', views.AddPost.as_view(), name='add_post'),
-    path('<int:year>/<int:month>/<slug:slug>/edit/', views.PostEditView.as_view(), name='post_edit')
+    path('<int:year>/<int:month>/<slug:slug>/edit/', views.PostEditView.as_view(), name='post_edit'),
+    path('<int:year>/<int:month>/<slug:slug>/delete/', views.PostDelete.as_view(), name='post_delete')
 ]
