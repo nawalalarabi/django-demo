7eb35b0fa52e5b51592c40997cffde6bb95f9a2e
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-03-07 16:09:17 +0200

    Add PostEdit

--- forum/urls.py
+++ forum/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('<int:year>/<int:month>/<slug:slug>/', views.PostShow.as_view(), name='post'),
     path('post/add/', views.AddPost.as_view(), name='add_post'),
+    path('<int:year>/<int:month>/<slug:slug>/edit/', views.PostEditView.as_view(), name='post_edit')
 ]
